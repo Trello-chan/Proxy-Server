a042443f13d786a9610b5dee2167bdb20fcc3e65
Kin Chan <xkinchanx@gmail.com>
2019-03-05 13:42:45 -0800

    [prog] proxy server, serving html that downloads required byndles

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import path from 'path';
+import parser from 'body-parser';
+
+// import router from './router';
+
+const app = express();
+const port = process.env.PORT || 3009;
+
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public/')));
+
+// app.use('/api', router)
+
+app.use('*', (req, res) => res.sendFile(path.resolve(__dirname, '../public/index.html')));
+
+if (process.env.NODE_ENV !== 'jest') {
+  app.listen(port, () => console.log(`server is listening on port ${port}`));
+}
+
+
+export default app;
