2951efa1c3154e36f4e6ede2c9b655b6c0cac5e2
Kin Chan <xkinchanx@gmail.com>
2019-03-13 12:24:26 -0700

    [prog] proxying requests to Search server on card data request.  Card data is rendering

--- server/searchRouter.js
+++ server/searchRouter.js
@@ -5,15 +5,24 @@ const router = express.Router();
 
 router.route('/load')
   .get((req, res) => {
-    console.log('hitting router')
     let { id } = req.query;
     let url = process.env.ENV === 'production' ? process.env.SEARCH_SERVER : `localhost:3001`;
-    console.log(url)
     axios
       .get(`http://${url}/search-api/load?id=${id}`)
       .then(({data}) => res.status(200).send(data))
       .catch(() => res.status(404).send('error fetching from Search server'))
   })
+
+router.route('/card')
+  .get((req, res) => {
+    let { label } = req.query;
+    let url = process.env.ENV === 'production' ? process.env.SEARCH_SERVER : `localhost:3001`;
+    axios
+      .get(`http://${url}/search-api/card?label=${label}`)
+      .then(({data}) => res.status(200).send(data))
+      .catch(() => res.status(404).send('error fetching from Search server'))
+  })
+
 
 //if this works, we'll need one for 
 //      .get(`/search-api/card?label=${label}`)
